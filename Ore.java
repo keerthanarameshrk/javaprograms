4cafcacbd4b4455f3e2b0a7ad9e4f632d139d07f
keerthanarameshrk <50692458+keerthanarameshrk@users.noreply.github.com>
2019-05-27 21:26:43 +0530

    Add files via upload

---
+++ Ore.java
@@ -0,0 +1,14 @@
+//odd or even
+import java.util.Scanner;
+class Ore
+{
+public static void main(String args[])
+{
+int num;
+Scanner sc=new Scanner(System.in);
+num=sc.nextInt();
+if(num%2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+}}
