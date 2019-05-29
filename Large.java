ab98eb7cf8e15bdfa7bbbeeefe7f87418b90e229
keerthanarameshrk <50692458+keerthanarameshrk@users.noreply.github.com>
2019-05-29 10:58:02 +0530

    Add files via upload

---
+++ Large.java
@@ -0,0 +1,13 @@
+//largest of 3 numbers
+class Large
+{
+public static void main(String args[])
+{
+int n1=1,n2=2,n3=3;
+if(n1>n2)
+System.out.println("n1 is greater");
+else if(n2>n3)
+System.out.println("n2 is greater");
+else
+System.out.println("n3 is greater");
+}}
