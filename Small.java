29edac98d0b946ca61982402172af84ca4774293
keerthanarameshrk <50692458+keerthanarameshrk@users.noreply.github.com>
2019-05-29 11:16:16 +0530

    Add files via upload

---
+++ Small.java
@@ -0,0 +1,13 @@
+//smallest of 3 numbers
+class Small
+{
+public static void main(String args[])
+{
+int n1=1,n2=2,n3=3;
+if(n1<n2)
+System.out.println("n1 is smaller");
+else if(n2<n3)
+System.out.println("n2 is samller");
+else
+System.out.println("n3 is smaller");
+}}
