557370bc1fc175687cd6bedf522ce7dd60ff314b
keerthanarameshrk <50692458+keerthanarameshrk@users.noreply.github.com>
2019-07-01 10:54:54 +0530

    Add files via upload

---
+++ Exam.java
@@ -0,0 +1,18 @@
+class Exam
+{
+int marks;
+void result()
+{
+Stud s1=new Stud();
+s1.rollno=100;
+s1.name="rk";
+s1.marks=51;
+s1.display();
+if(s1.marks>50)
+{
+System.out.println("PASS");
+}
+else
+{
+System.out.println("fail");
+}}}
