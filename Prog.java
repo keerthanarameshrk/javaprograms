5a2d7e78da1dcc65ea8e533d4c3b63ee29a8f296
keerthanarameshrk <50692458+keerthanarameshrk@users.noreply.github.com>
2019-07-01 10:59:48 +0530

    Add files via upload

---
+++ Prog.java
@@ -0,0 +1,12 @@
+class Prog
+{
+public static void main(String args[])
+{
+int i,n=10,j;
+for(i=1;i<=n;++i)
+{
+for(j=1;j<=i;++j)
+{
+System.out.print(i);
+}
+System.out.println(" ");}}}
---
+++ Progs.java
@@ -0,0 +1,12 @@
+class Progs
+{
+public static void main(String args[])
+{
+int i,n=10,j;
+for(i=1;i<=n;++i)
+{
+for(j=1;j<=i;j++)
+{
+System.out.print(j);
+}
+System.out.println(" ");}}}
