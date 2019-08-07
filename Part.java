1ac7a368cc957e5700cd1b2b4ff6b5b720ee8e44
keerthanarameshrk <50692458+keerthanarameshrk@users.noreply.github.com>
2019-08-07 12:08:47 +0530

    Add files via upload

---
+++ Part.java
@@ -0,0 +1,17 @@
+class Part
+{
+public static void main(String ars[])
+{
+int rows=0;
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=rows-i;j++)
+{
+System.out.print(1);
+}
+for(int j=1;j<=i;j++)
+{
+System.out.println(i);
+}
+System.out.println();
+}}}
---
+++ Parts.java
@@ -0,0 +1,16 @@
+class Parts
+{
+public static void main(String ars[])
+{
+for(int i=1;i<=6;i++)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print(j+" ");
+}
+for(int j=1;j>=1;j--)
+{
+System.out.println(j+" ");
+}
+System.out.println();
+}}}
